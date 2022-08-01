c429fb5e9ff036c766ecbd7767b49caa56ad401b
AnuragS13 <sharma.akashanurag@gmail.com>
2022-08-02 01:40:59 +0530

    added partial update function

--- item/views.py
+++ item/views.py
@@ -9,7 +9,18 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ItemView(viewsets.ModelViewSet):
+    http_method_names = ['get', 'post', 'put']
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ('name', 'category', 'subcategory')
+
+    def put(self, request):
+        return self.partial_update(request)
+
+    def partial_update(self, request):
+        instance = self.queryset.get(pk=request.GET['id'])
+        serializer = self.serializer_class(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
