0feddd5154fb4ae244619a5247d87f620812d4f8
AnuragS13 <sharma.akashanurag@gmail.com>
2022-08-01 23:28:45 +0530

    removed unnecessary dependency

--- item/admin.py
+++ item/admin.py
@@ -3,13 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
-from search_admin_autocomplete.admin import SearchAutoCompleteAdmin
 
 from .models import Item
 from .resources import ItemResource
 
 
 @admin.register(Item)
-class UserAdmin(ImportExportModelAdmin, SearchAutoCompleteAdmin):
+class UserAdmin(ImportExportModelAdmin):
     resource_class = ItemResource
     search_fields = ['name', 'category', 'subcategory']
